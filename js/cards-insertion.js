27d884987c261b3c1eddd5a3f0d1c89fe32fdd7e
FlautistaMacabro <vinicius.pelegrineli@unesp.br>
2023-11-24 20:46:34 -0300

    feat: Shopping cart itens style added

--- js/cards-insertion.js
+++ js/cards-insertion.js
@@ -4,33 +4,33 @@ window.addEventListener("load", () => {
   const cardsInfoList = [
     {
       title: "Lightweight Jacket",
-      image: "img/lightweight-jacket.jpg",
+      // image: "img/lightweight-jacket.jpg",
       description: "Adicione um pouco de energia ao seu guarda roupa de inverno com esta jaqueta vibrante..."
     }
     ,
     {
       title: "Elegance Jacket",
-      image: "img/elegance-jacket.jpg",
+      // image: "img/elegance-jacket.jpg",
       description: "Roupas elegantes para eventos especiais, trazendo sofisticação e uma boa impressão de primeira..."
     },
     {
       title: "Over Shirt",
-      image: "img/over-shirt.jpg",
+      // image: "img/over-shirt.jpg",
       description: "Adicione muita energia ao seu guarda roupa do dia a dia com esta camiseta sensacional..."
     },
     {
       title: "Super Over Shirt",
-      image: "img/super-over-shirt.jpg",
+      // image: "img/super-over-shirt.jpg",
       description: "Adicione muita energia ao seu guarda roupa do dia a dia com esta camiseta sensacional..."
     },
     {
       title: "Minimal Jacket",
-      image: "img/minimal-jacket.jpg",
+      // image: "img/minimal-jacket.jpg",
       description: "Adicione elegância e estilo em seu guarda roupa com nossas camiseta de manga longa basicas..."
     },
     {
       title: "Weather Shirts",
-      image: "img/weather-shirts.jpg",
+      // image: "img/weather-shirts.jpg",
       description: "Cores vibrantes e uma energia sem igual é o que você precisa ter no seu guardada roupa de primavera..."
     }
   ];
@@ -38,7 +38,7 @@ window.addEventListener("load", () => {
   const cardsContainer = document.getElementById("cards__container");
   cardsInfoList.forEach((cardInfo) => {
     let card = `<div class="card">
-                  <img class="card__image" src="${cardInfo.image}" alt="${cardInfo.title}">
+                  // <img class="card__image" src="${cardInfo.image}" alt="${cardInfo.title}">
                   <div class="card__details__container">
                     <div class="card__type"> ${cardsType} </div>
                     <strong class="card__title"> ${cardInfo.title} </strong>
